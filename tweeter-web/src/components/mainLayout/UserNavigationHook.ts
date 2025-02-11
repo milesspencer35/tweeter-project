839bc61b86a015cae68001e29feaa8332e9bcd96
Miles Spencer <milesspencer35@gmail.com>
2025-02-11 16:43:15 -0700

    Refactor UserNavigationHook to use UserPresenter

--- tweeter-web/src/components/mainLayout/UserNavigationHook.ts
+++ tweeter-web/src/components/mainLayout/UserNavigationHook.ts
@@ -1,6 +1,7 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/UserInfoHook";
+import { useState } from "react";
+import { UserView, UserPresenter } from "../../presenters/UserPresenter";
 
 interface UserNavigation {
     navigateToClickedUser: (event: React.MouseEvent) => void;
@@ -14,37 +15,17 @@ const useUserNavigation = (): UserNavigation => {
     const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
         event.preventDefault();
 
-        try {
-            const alias = extractAlias(event.target.toString());
-
-            const user = await getUser(authToken!, alias);
-
-            if (!!user) {
-                if (currentUser!.equals(user)) {
-                    setDisplayedUser(currentUser!);
-                } else {
-                    setDisplayedUser(user);
-                }
-            }
-        } catch (error) {
-            displayErrorMessage(
-                `Failed to get user because of exception: ${error}`
-            );
-        }
+        presenter.navigateToUser(event.target.toString());
     };
 
-    const extractAlias = (value: string): string => {
-        const index = value.indexOf("@");
-        return value.substring(index);
-    };
-
-    const getUser = async (
-        authToken: AuthToken,
-        alias: string
-    ): Promise<User | null> => {
-        // TODO: Replace with the result of calling server
-        return FakeData.instance.findUserByAlias(alias);
-    };
+    const listener: UserView = {
+          displayErrorMessage: displayErrorMessage,
+          authToken: authToken,
+          currentUser: currentUser,
+          setDisplayedUser: setDisplayedUser
+      }
+    
+      const [presenter] = useState(new UserPresenter(listener));
 
     return {
         navigateToClickedUser: (event: React.MouseEvent) => navigateToUser(event),
