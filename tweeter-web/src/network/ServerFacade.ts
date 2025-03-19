fb0c2ed89915bd93ad36bdcb7d4eefc9d7592452
Miles Spencer <milesspencer35@gmail.com>
2025-03-19 17:34:21 -0600

    Delete commented out code that we refactored out

--- tweeter-web/src/network/ServerFacade.ts
+++ tweeter-web/src/network/ServerFacade.ts
@@ -17,28 +17,6 @@ export class ServerFacade {
     public async getMoreFollowees(
         request: PagedUserItemRequest<UserDto>
     ): Promise<[User[], boolean]> {
-        // const response = await this.clientCommunicator.doPost<
-        //     PagedUserItemRequest<UserDto>,
-        //     PagedUserItemResponse<UserDto>
-        // >(request, "/followee/list");
-
-        // // Convert the UserDto array returned by ClientCommunicator to a User array
-        // const items: User[] | null =
-        //     response.success && response.items
-        //         ? response.items.map((dto) => User.fromDto(dto) as User)
-        //         : null;
-
-        // // Handle errors
-        // if (response.success) {
-        //     if (items == null) {
-        //         throw new Error(`No followees found`);
-        //     } else {
-        //         return [items, response.hasMore];
-        //     }
-        // } else {
-        //     console.error(response);
-        //     throw new Error(response.message ?? undefined);
-        // }
 
 		return this.morePagedUserItems<User, UserDto>(request, "/followee/list", `No followee found`, User);
     }
@@ -46,52 +24,13 @@ export class ServerFacade {
     public async getMoreFollowers(
         request: PagedUserItemRequest<UserDto>
     ): Promise<[User[], boolean]> {
-        // const response = await this.clientCommunicator.doPost<
-        //     PagedUserItemRequest<UserDto>,
-        //     PagedUserItemResponse<UserDto>
-        // >(request, "/follower/list");
 
-        // const items: User[] | null =
-        //     response.success && response.items
-        //         ? response.items.map((dto) => User.fromDto(dto) as User)
-        //         : null;
-
-        // if (response.success) {
-        //     if (items == null) {
-        //         throw new Error(`No followers found`);
-        //     } else {
-        //         return [items, response.hasMore];
-        //     }
-        // } else {
-        //     console.error(response);
-        //     throw new Error(response.message ?? undefined);
-        // }
 		return this.morePagedUserItems<User, UserDto>(request, "/follower/list", `No followers found`, User);
     }
 
     public async loadMoreFeedItems(
         request: PagedUserItemRequest<StatusDto>
     ): Promise<[Status[], boolean]> {
-        // const response = await this.clientCommunicator.doPost<
-        //     PagedUserItemRequest<StatusDto>,
-        //     PagedUserItemResponse<StatusDto>
-        // >(request, "/feed/list");
-
-        // const items: Status[] | null =
-        //     response.success && response.items
-        //         ? response.items.map((dto) => Status.fromDto(dto) as Status)
-        //         : null;
-
-        // if (response.success) {
-        //     if (items == null) {
-        //         throw new Error(`No feed found`);
-        //     } else {
-        //         return [items, response.hasMore];
-        //     }
-        // } else {
-        //     console.error(response);
-        //     throw new Error(response.message ?? undefined);
-        // }
 
 		return this.morePagedUserItems<Status, StatusDto>(request, "/feed/list", `No story found`, Status);
     }
@@ -99,26 +38,7 @@ export class ServerFacade {
     public async loadMoreStoryItems(
         request: PagedUserItemRequest<StatusDto>
     ): Promise<[Status[], boolean]> {
-        // const response = await this.clientCommunicator.doPost<
-        //     PagedUserItemRequest<StatusDto>,
-        //     PagedUserItemResponse<StatusDto>
-        // >(request, "/story/list");
 
-        // const items: Status[] | null =
-        //     response.success && response.items
-        //         ? response.items.map((dto) => Status.fromDto(dto) as Status)
-        //         : null;
-
-        // if (response.success) {
-        //     if (items == null) {
-        //         throw new Error(`No story found`);
-        //     } else {
-        //         return [items, response.hasMore];
-        //     }
-        // } else {
-        //     console.error(response);
-        //     throw new Error(response.message ?? undefined);
-        // }
 		return this.morePagedUserItems<Status, StatusDto>(request, "/story/list", `No story found`, Status);
     }
 
